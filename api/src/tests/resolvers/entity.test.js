5244b6a5e3ada3126cfafb4b2444c1869b573b14
Marc Litchfield <marclitchfield@gmail.com>
2018-08-02 21:39:28 -0700

    Rename Query tests to Entity tests. Move into resolvers folder

--- api/src/tests/resolvers/entity.test.js
+++ api/src/tests/resolvers/entity.test.js
@@ -1,11 +1,11 @@
-import { applySetup, verifyMutations } from './test-setup';
+import { applySetup, verifyMutations } from '../test-setup';
 import gql from 'graphql-tag';
 
-describe('Query tests', () => {
+describe('Entity tests', () => {
   applySetup();
 
   test('simple post (idea in space)', () => {
-    return verifyMutations('Query tests', 'idea in space', {
+    return verifyMutations('Entity tests', 'idea in space', {
       mutations: [
         ({space}) => gql`mutation { Idea { idea:post_in_space(input:{sourceId:"${space.id}", title: "test idea"}) { id } } }`
       ],
@@ -16,7 +16,7 @@ describe('Query tests', () => {
   });
 
   test('post entity with beforeMerge (event in space)', () => {
-    return verifyMutations('Query tests', 'event in space', {
+    return verifyMutations('Entity tests', 'event in space', {
       mutations: [
         ({space}) => gql`mutation { Event { event:post_in_space(input:{sourceId:"${space.id}", title: "test event", datetime: "2011-12-12"}) { id } } }`
       ],
@@ -27,7 +27,7 @@ describe('Query tests', () => {
   });
 
   test('post instance (item in space)', () => {
-    return verifyMutations('Query tests', 'item in space', {
+    return verifyMutations('Entity tests', 'item in space', {
       mutations: [
         ({space}) => gql`mutation { Concept { concept:post_in_space(input:{sourceId:"${space.id}", title: "test concept"}) { id } } }`,
         ({space, concept}) => gql`mutation { Item { item:post_in_space(input:{sourceId:"${space.id}", classId:"${concept.id}", title: "test item"}) { id } } }`
@@ -39,7 +39,7 @@ describe('Query tests', () => {
   });
 
   test('post single cardinality (person at location)', () => {
-    return verifyMutations('Query tests', 'person at location', {
+    return verifyMutations('Entity tests', 'person at location', {
       mutations: [
         ({space}) => gql`mutation { Location { location:post_in_space(input:{sourceId:"${space.id}", title: "test location"}) { id } } }`,
         ({location}) => gql`mutation { Person { person:post_at_location(input:{sourceId:"${location.id}", title: "test person"}) { id } } }`
@@ -51,7 +51,7 @@ describe('Query tests', () => {
   });
 
   test('post single cardinality update (update person location)', () => {
-    return verifyMutations('Query tests', 'update person location', {
+    return verifyMutations('Entity tests', 'update person location', {
       mutations: [
         ({space}) => gql`mutation { Location { location1:post_in_space(input:{sourceId:"${space.id}", title: "test location 1"}) { id } } }`,
         ({space}) => gql`mutation { Location { location2:post_in_space(input:{sourceId:"${space.id}", title: "test location 2"}) { id } } }`,
@@ -65,7 +65,7 @@ describe('Query tests', () => {
   });
 
   test('post single cardinality instance (item at location)', () => {
-    return verifyMutations('Query tests', 'item at location', {
+    return verifyMutations('Entity tests', 'item at location', {
       mutations: [
         ({space}) => gql`mutation { Concept { concept:post_in_space(input:{sourceId:"${space.id}", title: "test concept"}) { id } } }`,
         ({space}) => gql`mutation { Location { location:post_in_space(input:{sourceId:"${space.id}", title: "test location"}) { id } } }`,
@@ -81,7 +81,7 @@ describe('Query tests', () => {
   });
 
   test('post properties (root concept in space)', () => {
-    return verifyMutations('Query tests', 'root concept in space', {
+    return verifyMutations('Entity tests', 'root concept in space', {
       mutations: [
         ({space}) => gql`mutation { Concept { rootConcept:post_in_space(input:{sourceId:"${space.id}", title: "root concept"}) { id } } }`,
         ({rootConcept}) => gql`mutation { Concept { leafConcept:post_sub_concept(input:{sourceId:"${rootConcept.id}", title: "sub concept"}) { id } } }`,
@@ -96,7 +96,7 @@ describe('Query tests', () => {
   });
 
   test('post single cardinality removal (remove event at location)', () => {
-    return verifyMutations('Query tests', 'remove event at location', {
+    return verifyMutations('Entity tests', 'remove event at location', {
       mutations: [
         ({space}) => gql`mutation { Location { location:post_in_space(input:{sourceId:"${space.id}", title: "test location"}) { id } } }`,
         ({location}) => gql`mutation { Person { person:post_at_location(input:{sourceId:"${location.id}", title: "test person"}) { id } } }`,
